fb2154164629723aea85d24371ca100e63425084
Görkem SAVRAN <gorkem.savran-p@intra.local>
2021-12-03 14:16:45 +0300

    LoginController integration testleri yazıldı.

---
+++ src/test/java/com/gorkemsavran/login/controller/LoginControllerIT.java
@@ -0,0 +1,52 @@
+package com.gorkemsavran.login.controller;
+
+import com.gorkemsavran.TestConfig;
+import com.gorkemsavran.user.entity.Authority;
+import com.gorkemsavran.user.entity.User;
+import com.gorkemsavran.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringJUnitWebConfig(classes = TestConfig.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class LoginControllerIT {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    LoginController loginController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(loginController).build();
+        User user = new User("admin", "admin", Authority.ROLE_ADMIN, "email");
+        userService.addUser(user);
+    }
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"username\": \"admin\",\n" +
+                        "    \"password\": \"admin\"\n" +
+                        "}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.token", is(String.class)));
+    }
+}
