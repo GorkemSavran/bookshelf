1b161380f284baca1258dde94b48fc91dc93c46b
Görkem SAVRAN <gorkem.savran-p@intra.local>
2021-12-10 09:54:40 +0300

    User'ın Authentication ve asıl domain kısımları ayrıldı. BaseUser yazılarak authentication ile ilgili bütün kısımlar buraya aktarıldı.

---
+++ src/main/java/com/gorkemsavran/user/entity/BaseUser.java
@@ -0,0 +1,86 @@
+package com.gorkemsavran.user.entity;
+
+import com.gorkemsavran.common.entity.BaseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.MappedSuperclass;
+import java.util.Collection;
+import java.util.Collections;
+
+@MappedSuperclass
+public abstract class BaseUser extends BaseEntity implements UserDetails {
+
+    @Column(unique = true)
+    protected String username;
+
+    protected String password;
+
+    @Enumerated(value = EnumType.STRING)
+    protected Authority role;
+
+    @Column(unique = true)
+    protected String email;
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(() -> getRole().toString());
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Authority getRole() {
+        return role;
+    }
+
+    public void setRole(Authority role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/gorkemsavran/user/entity/User.java
+++ src/main/java/com/gorkemsavran/user/entity/User.java
@@ -1,29 +1,18 @@
 package com.gorkemsavran.user.entity;
 
 import com.gorkemsavran.book.entity.Book;
-import com.gorkemsavran.common.entity.BaseEntity;
-import com.gorkemsavran.userbookshelf.controller.request.AddReviewAndRatingDTO;
 import com.gorkemsavran.userbookshelf.entity.Shelf;
 import com.gorkemsavran.userbookshelf.entity.UserBook;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
-import java.util.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-public class User extends BaseEntity implements UserDetails {
-
-    @Column(unique = true)
-    private String username;
-
-    private String password;
-
-    @Enumerated(value = EnumType.STRING)
-    private Authority role;
-
-    @Column(unique = true)
-    private String email;
+public class User extends BaseUser {
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "user")
     private Set<UserBook> userBooks;
@@ -75,31 +64,6 @@ public class User extends BaseEntity implements UserDetails {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(() -> getRole().toString());
-    }
-
-    @Override
     public String toString() {
         return "User{" +
                 "username='" + username + '\'' +
@@ -107,38 +71,6 @@ public class User extends BaseEntity implements UserDetails {
                 ", role=" + role +
                 ", email='" + email + '\'' +
                 '}';
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Authority getRole() {
-        return role;
-    }
-
-    public void setRole(Authority role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public Set<UserBook> getUserBooks() {
